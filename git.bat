7a9ee8fd0619142f337bcafd203fbd2fb97bef90
udpsec <udpsec@users.noreply.github.com>
2018-01-10 16:21:15 +0800

    update

--- git.bat
+++
@@ -1,4 +0,0 @@
-starred --username hackwith --sort > hackwith.md
-starred --username udpsec --sort > udpsec.md
-starred --username alchu4n --sort > alchu4n.md
-git add . && git commit -m "update" && git push
